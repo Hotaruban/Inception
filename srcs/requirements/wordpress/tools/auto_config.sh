8c9ac4d9a4bccf93e77d2606e540f471a61d9002
Hotaru <jeremyhurpy@gmail.com>
2024-10-07 00:26:19 +0700

    [FIX] change path of directory

--- srcs/requirements/wordpress/tools/auto_config.sh
+++ srcs/requirements/wordpress/tools/auto_config.sh
@@ -11,7 +11,7 @@ if mysqladmin ping -h mariadb --silent; then
 		--dbname=$MYSQL_DATABASE \
 		--dbuser=$MYSQL_USER \
 		--dbpass=$MYSQL_PASSWORD \
-		--dbhost=mariadb:3306 --path=/var/www/html/
+		--dbhost=mariadb:3306 \
 
 	wp core install --allow-root \
 		--url=$WORDPRESS_URL \
@@ -19,17 +19,15 @@ if mysqladmin ping -h mariadb --silent; then
 		--admin_user=$WORDPRESS_ADMIN_USER \
 		--admin_password=$WORDPRESS_ADMIN_PASSWORD \
 		--admin_email=$WORDPRESS_ADMIN_EMAIL \
-		--path=/var/www/html/
 
 	wp user create --allow-root \
 		$WORDPRESS_USER $WORDPRESS_USER_EMAIL \
 		--role=author \
 		--user_pass=$WORDPRESS_USER_PASSWORD \
-		--path=/var/www/html/
 
-	wp plugin update --all --allow-root --path=/var/www/html/
+	wp plugin update --all --allow-root
 
-	chown -R www-data:www-data /var/www/html
+	chown -R www-data:www-data
 
 	/usr/sbin/php-fpm7.4 -F
 
