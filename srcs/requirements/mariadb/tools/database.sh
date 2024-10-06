90677f6209a5659819c321c60cb87ad5afe091f0
Hotaru <jeremyhurpy@gmail.com>
2024-10-07 02:12:48 +0700

    [FIX] modify setup root password

--- srcs/requirements/mariadb/tools/database.sh
+++ srcs/requirements/mariadb/tools/database.sh
@@ -5,18 +5,23 @@
 /etc/init.d/mariadb start
 
 # secure the installation
-mysql_secure_installation<<EOF
-y
-$MYSQL_ROOT_PASSWORD
-$MYSQL_ROOT_PASSWORD
-y
-y
-y
-y
-EOF
+#mysql_secure_installation<<EOF
+#y
+#$MYSQL_ROOT_PASSWORD
+#$MYSQL_ROOT_PASSWORD
+#y
+#y
+#y
+#y
+#EOF
 
 # create the database and the user
 #mariadb -u root -p$MYSQL_ROOT_PASSWORD<<EOF
+#GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;
+#FLUSH PRIVILEGES;
+
+mariadb -u root <<EOF
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
@@ -25,7 +30,7 @@ GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
 FLUSH PRIVILEGES;
 EOF
 
-mysql -u root -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /tmp/wordpress.sql
+#mysql -u root -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /tmp/wordpress.sql
 
 # stop the service
 /etc/init.d/mariadb stop
