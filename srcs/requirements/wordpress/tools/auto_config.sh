1cd3c04dc44dc058a18d094dbdd6d65e9715cb62
Hotaru <jeremyhurpy@gmail.com>
2024-10-07 00:46:48 +0700

    [FIX] change auto config file

--- srcs/requirements/wordpress/tools/auto_config.sh
+++ srcs/requirements/wordpress/tools/auto_config.sh
@@ -5,17 +5,13 @@ sleep 10
 if mysqladmin ping -h mariadb --silent; then
 	echo "MariaDB is up and running!"
 
-	# Auto config for WordPress
-	if [ ! -f /var/www/html/wp-config.php ]; then
-		wp core download --allow-root --path='/var/www/html'
-	fi
-
+	# Auto config for wordpress
+	wp core download --allow-root
 	wp config create --allow-root \
 		--dbname=$MYSQL_DATABASE \
 		--dbuser=$MYSQL_USER \
 		--dbpass=$MYSQL_PASSWORD \
 		--dbhost=mariadb:3306 \
-		--path='/var/www/html'
 
 	wp core install --allow-root \
 		--url=$WORDPRESS_URL \
@@ -23,17 +19,15 @@ if mysqladmin ping -h mariadb --silent; then
 		--admin_user=$WORDPRESS_ADMIN_USER \
 		--admin_password=$WORDPRESS_ADMIN_PASSWORD \
 		--admin_email=$WORDPRESS_ADMIN_EMAIL \
-		--path='/var/www/html'
 
 	wp user create --allow-root \
 		$WORDPRESS_USER $WORDPRESS_USER_EMAIL \
 		--role=author \
 		--user_pass=$WORDPRESS_USER_PASSWORD \
-		--path='/var/www/html'
 
-	wp plugin update --all --allow-root --path='/var/www/html'
+	wp plugin update --all --allow-root
 
-	chown -R www-data:www-data /var/www/html
+	chown -R www-data:www-data
 
 	/usr/sbin/php-fpm8.2 -F
 
