c777784b74c2c5d6499a698b85ffea9f800eefd4
Hotaru <jeremyhurpy@gmail.com>
2024-10-07 16:56:16 +0700

    [FIX] modify script

--- srcs/requirements/mariadb/tools/database.sh
+++ srcs/requirements/mariadb/tools/database.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+##!/bin/bash
 set -e
 
 MAX_TRIES=30
@@ -13,29 +13,64 @@ while ! mysqladmin ping -h"localhost" --silent; do
 	fi
 done
 
-echo "MariaDB is ready. Proceeding with initialization..."
+echo "MySQL is ready."
+
+## script to create the database and the user
+#sleep 10
+
+##MYSQL_ROOT_PASSWORD="42root"
+##MYSQL_DATABASE=wordpress
+##MYSQL_PASSWORD=42userjdg
+##MYSQL_USER=userjdg
+
+# secure the installation
+mariadb -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+
+mariadb -u root -p"$MYSQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
+
+# delete the anonymous user
+mariadb -u root -p"$MYSQL_ROOT_PASSWORD" -e "DELETE FROM mysql.user WHERE User='';"
+
+# delete the remote root user
+mariadb -u root -p"$MYSQL_ROOT_PASSWORD" -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
+
+# delete the test database
+mariadb -u root -p"$MYSQL_ROOT_PASSWORD" -e "DROP DATABASE IF EXISTS test;"
+mariadb -u root -p"$MYSQL_ROOT_PASSWORD" -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';"
+
+# reload the privileges
+mariadb -u root -p"$MYSQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
+
+mariadb -u root -p"$MYSQL_ROOT_PASSWORD" <<EOF
+CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
+CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
+FLUSH PRIVILEGES;
+EOF
+
+
+
+#!/bin/bash
 
 # Secure the installation
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
-mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
+#mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+#mariadb -e "DELETE FROM mysql.user WHERE User='';"
+#mariadb -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
+#mariadb -e "DROP DATABASE IF EXISTS test;"
+#mariadb -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';"
+#mariadb -e "FLUSH PRIVILEGES;"
 
 # Create the database and the user
-mysql -u root -p"${MYSQL_ROOT_PASSWORD}" <<-EOSQL
-    DELETE FROM mysql.user WHERE User='';
-    DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-    DROP DATABASE IF EXISTS test;
-    DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
-    FLUSH PRIVILEGES;
-EOSQL
+#mariadb -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
+#mariadb -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+#mariadb -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
+#mariadb -e "FLUSH PRIVILEGES;"
 
 echo "Database initialization completed."
-
-# Debug
 #echo "SHOW DATABASES"
 #mariadb -u root -p"$MYSQL_ROOT_PASSWORD" -e "SHOW DATABASES;"
 #echo "SHOW USERS"
 #mariadb -u root -p"$MYSQL_ROOT_PASSWORD" -e "SELECT User FROM mysql.user;"
 #echo "SHOW GRANTS"
 #mariadb -u root -p"$MYSQL_ROOT_PASSWORD" -e "SHOW GRANTS FOR '$MYSQL_USER'@'%';"
+
