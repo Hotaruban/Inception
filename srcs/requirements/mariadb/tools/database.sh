0993702f32f3abcdc855ad7a69b8d90dda53de75
Hotaru <jeremyhurpy@gmail.com>
2024-10-07 16:48:21 +0700

    [FIX] modify script

--- srcs/requirements/mariadb/tools/database.sh
+++ srcs/requirements/mariadb/tools/database.sh
@@ -13,27 +13,29 @@ while ! mysqladmin ping -h"localhost" --silent; do
 	fi
 done
 
-echo "MySQL is ready."
+echo "MariaDB is ready. Proceeding with initialization..."
 
 # Secure the installation
-mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-mariadb -e "DELETE FROM mysql.user WHERE User='';"
-mariadb -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
-mariadb -e "DROP DATABASE IF EXISTS test;"
-mariadb -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';"
-mariadb -e "FLUSH PRIVILEGES;"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
+mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
 
 # Create the database and the user
-mariadb -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
-mariadb -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mariadb -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
-mariadb -e "FLUSH PRIVILEGES;"
+mysql -u root -p"${MYSQL_ROOT_PASSWORD}" <<-EOSQL
+    DELETE FROM mysql.user WHERE User='';
+    DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+    DROP DATABASE IF EXISTS test;
+    DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+    FLUSH PRIVILEGES;
+EOSQL
 
 echo "Database initialization completed."
+
+# Debug
 #echo "SHOW DATABASES"
 #mariadb -u root -p"$MYSQL_ROOT_PASSWORD" -e "SHOW DATABASES;"
 #echo "SHOW USERS"
 #mariadb -u root -p"$MYSQL_ROOT_PASSWORD" -e "SELECT User FROM mysql.user;"
 #echo "SHOW GRANTS"
 #mariadb -u root -p"$MYSQL_ROOT_PASSWORD" -e "SHOW GRANTS FOR '$MYSQL_USER'@'%';"
-
