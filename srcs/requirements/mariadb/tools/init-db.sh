0b16017f50163164ca25dee524f9644133c088b0
Hotaru <jeremyhurpy@gmail.com>
2024-10-08 01:40:49 +0700

    [FIX] change script for restart docker

--- srcs/requirements/mariadb/tools/init-db.sh
+++ srcs/requirements/mariadb/tools/init-db.sh
@@ -7,8 +7,19 @@ mysqld --skip-networking &
 MAX_ATTEMPTS=12
 attempt=0
 
+
+# Check if the root password is not set
+check_mariadb_without_password() {
+	mariadb -u root -e "SELECT 1;" >/dev/null 2>&1
+}
+
+# Check if the root password is set
+check_mariadb_with_password() {
+	mariadb -u root -p"$MYSQL_ROOT_PASSWORD" -e "SELECT 1;" >/dev/null 2>&1
+}
+
 # Wait for MariaDB to be ready (retry up to MAX_ATTEMPTS times)
-until mariadb -u root -e "SELECT 1;" >/dev/null 2>&1; do
+until check_mariadb_without_password || check_mariadb_with_password; do
 	attempt=$((attempt+1))
 	if [ $attempt -ge $MAX_ATTEMPTS ]; then
 		echo "MariaDB did not become ready in time, exiting..."
@@ -20,8 +31,14 @@ done
 
 echo "MariaDB is ready."
 
-# Run the database initialization script
-/usr/local/bin/database.sh
+# Check if the database already exists
+if mariadb -u root -e "USE $MYSQL_DATABASE;" >/dev/null 2>&1; then
+	echo "Database $MYSQL_DATABASE already exists. Skipping initialization."
+else
+	echo "Database $MYSQL_DATABASE does not exist. Running initialization script."
+	# Run the database initialization script
+	/usr/local/bin/database.sh
+fi
 
 # Restart MariaDB with networking enabled to allow external connections
 killall mysqld
